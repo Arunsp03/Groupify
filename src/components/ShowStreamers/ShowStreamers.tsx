cc706c2b8bb5261b1032f31de2aef27d2ac36fc3
Arunsp03 <arun2002362002@gmail.com>
2024-11-02 20:29:43 +0530

    search functionality in show streamers screen

--- src/components/ShowStreamers/ShowStreamers.tsx
+++ src/components/ShowStreamers/ShowStreamers.tsx
@@ -7,21 +7,42 @@ const {getStreamerList}=Apiservice
 const ShowStreamers=()=>{
     const router=useRouter()
     const [streamers,setStreamers]=useState([])
-  
+  const[searchStreamer,setSearchStreamer]=useState("")
+  const [filteredStreamers,setFilteredStreamers]=useState([])
     const {selectedstreamer,setSelectedStreamer}=useContext(statecontext)
     useEffect(()=>{
         
         const fetchData=async()=>{
             const streamers=await getStreamerList();
             setStreamers(streamers);
+setFilteredStreamers(streamers);
+            
         }
         fetchData();
     },[])
+    const filterStreamer=()=>{
+        if(searchStreamer=="")
+            {
+                setFilteredStreamers(streamers)
+            }
+            else{
+            setFilteredStreamers(streamers.filter((streamer:any)=>streamer.streamername==searchStreamer))
+            }
+    }
+
 return (
+    <>
+    <div>
+         <input className="text-black  w-[20vw] h-[6vh] font-semibold rounded-sm ml-10 mt-10 text-center p-3" value={searchStreamer} onChange={(e)=>{
+            setSearchStreamer(e.target.value)
+           
+         }} placeholder={"Enter streamer name"} type="text"/>
+         <button type="button" className="ml-2 p-3 font-semibold hover:bg-white hover:text-black rounded-md" onClick={filterStreamer}>Search</button>
+    </div>
     <div className="text-white grid grid-cols-4 w-[80vw] ml-3 mt-[3rem]"  >
        {/* <p className="text-white">{selectedstreamer}</p> */}
-       
-        {streamers && streamers.map((item:any,index)=>{
+  
+        {filteredStreamers && filteredStreamers.map((item:any,index)=>{
             return(
                 <div key={index} className="bg-blue-500 w-[18vw] h-[20vh]  rounded-sm flex flex-col justify-between m-[1rem]">
                 <p className="font-semibold text-center m-7 text-xl">{item.streamername}</p>
@@ -35,6 +56,7 @@ return (
             )
         })}
     </div>
+    </>
 )
 }
 export default ShowStreamers;
